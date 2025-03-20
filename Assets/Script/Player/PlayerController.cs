0d3abcc60402fc09895eb77f5507fdfe4e2de137
ChoiChiWon <ckw0655@kookmin.ac.kr>
2025-03-21 03:22:55 +0900

    Create Player
    Create Layer "Player"
    
    PlayerController.cs
    1. move camera by 'wasd'
    2. rotate camera by 'q,e'
    3. zoom camera by mouse wheel
    
    RestrictBoundary.cs is a script that prevents the player from leaving the designated boundary.

---
+++ Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Cinemachine;
+
+public class PlayerController : MonoBehaviour
+{
+    [System.Serializable]
+    public struct Boundary1D
+    {
+        public float min;
+        public float max;
+
+        public Boundary1D(int low = 0, int high = 0)
+        {
+            min = low; max = high;
+        }
+    }
+
+    //virtual Camera Options
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
+    CinemachineComponentBase componentBase;
+
+    [SerializeField] InputActionReference IA_Move;
+    [SerializeField] InputActionReference IA_Zoom;
+    [SerializeField] InputActionReference IA_Rotate;
+
+    public float moveSpeed;
+    public float zoomSensitivity;
+    public Boundary1D zoomBoundary = new Boundary1D(2, 10);
+    public float rotationSpeed;
+    public float rotationResetSpeed;
+    public float rotationResetAngle;
+
+    void Start()
+    {
+        if (componentBase == null)
+        {
+            componentBase = virtualCamera.GetCinemachineComponent(CinemachineCore.Stage.Body);
+        }
+    }
+
+    void Update()
+    {
+        MoveByInputAction();
+        ZoomByInputAction();
+        RotateByInputAction();
+    }
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void MoveByInputAction()
+    {
+        if (IA_Move == null)
+            return;
+
+        Vector2 moveInputVal = IA_Move.action.ReadValue<Vector2>();
+        Vector3 moveDirection = new Vector3(moveInputVal.x, 0, moveInputVal.y).normalized;
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+    private void ZoomByInputAction()
+    {
+        if(IA_Zoom == null)
+            return;
+
+        float zoomInputVal = IA_Zoom.action.ReadValue<float>();
+        if (zoomInputVal != 0)
+        {
+            if (componentBase is CinemachineFramingTransposer transposer)
+            {
+                transposer.m_CameraDistance = Mathf.Clamp(
+                    transposer.m_CameraDistance - zoomInputVal * zoomSensitivity * Time.deltaTime,
+                    zoomBoundary.min,
+                    zoomBoundary.max
+                );
+            }
+        }
+    }
+    private void RotateByInputAction()
+    {
+        if (IA_Rotate == null)
+            return;
+        float rotateInputVal = IA_Rotate.action.ReadValue<float>();
+
+        if (rotateInputVal != 0)
+        {
+            transform.Rotate(Vector3.up * rotateInputVal * rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            //Get Current YAngle (Translate to -180 ~ 180)
+            float currentYAngle = transform.eulerAngles.y;
+            if (currentYAngle > 180) currentYAngle -= 360;
+
+            // if YAngle is lower than rotationResetAngle slowly Turn to ZeroAngle
+            if (Mathf.Abs(currentYAngle) < rotationResetAngle)
+            {
+                float newRotation = Mathf.Lerp(currentYAngle, 0, rotationResetSpeed * Time.deltaTime);
+                transform.rotation = Quaternion.Euler(0, newRotation, 0);
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Script/Player/RestrictBoundary.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class RestrictBoundary : MonoBehaviour
+{
+    private BoxCollider boundsCollider;
+    private Bounds bounds;
+    [SerializeField] Transform player;
+
+    private void Start()
+    {
+        boundsCollider = GetComponent<BoxCollider>();
+        bounds = boundsCollider.bounds;
+    }
+
+    private void LateUpdate()
+    {
+        if (player == null) return;
+
+        Vector3 clampedPosition = player.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, bounds.min.x, bounds.max.x);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, bounds.min.y, bounds.max.y);
+        clampedPosition.z = Mathf.Clamp(clampedPosition.z, bounds.min.z, bounds.max.z);
+
+        player.position = clampedPosition;
+    }
+
+    private void OnValidate()
+    {
+        // bounds values synchronize with Editor
+        if (boundsCollider == null)
+            boundsCollider = GetComponent<BoxCollider>();
+
+        bounds = boundsCollider.bounds;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (boundsCollider == null)
+            boundsCollider = GetComponent<BoxCollider>();
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(boundsCollider.bounds.center, boundsCollider.bounds.size);
+    }
+}
