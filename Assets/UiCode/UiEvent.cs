6a0fd59935bfb3a4892ceda4e5855972cd70a259
YuMinSeok <sksals4c@gmail.com>
2025-03-26 23:36:40 +0900

    DefaultUi 완성 에셋필요

--- Assets/UiCode/UiEvent.cs
+++ Assets/UiCode/UiEvent.cs
@@ -1,11 +1,18 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class UiEvent : MonoBehaviour
+public class UiEvent : MonoBehaviour, IPointerClickHandler
 {
     public string targetName;
-    public bool active;
+    public bool active = false;
 
     private void OnMouseDown() // object ´­·¶À»¶§
+    {
+        onClick();
+    }
+    
+    public void OnPointerClick(PointerEventData eventData)
     {
         onClick();
     }
