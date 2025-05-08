5dc447bb96307e9b045f0f41e79db6bee0c8b789
kangmunjeong <kmj7232@gmail.com>
2025-05-08 15:41:44 +0900

    ListIdx 수정중

--- Assets/UiCode/ListIdx.cs
+++ Assets/UiCode/ListIdx.cs
@@ -9,6 +9,9 @@ public class ListIdx : MonoBehaviour
     public Slider mpSlider;
     public Sprite unitImg;
     public TMP_Text unitName;
+    public TMP_Text hpText;
+    public TMP_Text mpText;
+
     public void Init(CharacterStat data)
     {
         unitImg = transform.GetChild(0).gameObject.GetComponent<Sprite>();
@@ -17,11 +20,16 @@ public class ListIdx : MonoBehaviour
         unitData = data;
         hpSlider = sliders[0];
         mpSlider = sliders[1];
+        hpText = transform.GetChild(1).gameObject.GetComponent<TMP_Text>();
+        hpText = transform.GetChild(2).gameObject.GetComponent<TMP_Text>();
 
         //unitImg = data.unitImage;
         unitName.text = this.name = data.DisplayName;
         hpSlider.value = (float)data.hp / data.hp_max;
         mpSlider.value = (float)data.mp / data.mp_max;
+
+        hpText.text = $"{data.hp} / {data.hp_max}";
+        mpText.text = $"{data.mp} / {data.mp_max}";
     }
 
     public void SetParent(Transform parent)
