50e49acceda03e9eb4d11d4bd98deaa43dad92d6
YuMinSeok <sksals4c@gmail.com>
2025-05-13 19:55:44 +0900

    enemylist 출력, hero4개 이상 불가

--- Assets/UiCode/PositionGrid.cs
+++ Assets/UiCode/PositionGrid.cs
@@ -3,37 +3,32 @@ using UnityEngine.UI;
 
 public class PositionGrid : MonoBehaviour
 {
-    public string[] positionData = new string[9];
     [SerializeField]
-    Image startButtonimage;
+    Image buttonimage;
     int heroCount;
+    Transform[] slots;
 
-    public string[] GetPosition()
-    {
-        for (int i = 0; i < 9; i++)
-        {
-            positionData[i] = transform.GetChild(i).name;
-        }
-        return positionData;
-    }
+
+
+
 
     void HeroCounting()
     {
         heroCount = 0;
         for (int i = 0; i < 9; i++)
         {
-            if (positionData[i] != null)
+            if (slots[i].childCount > 0)
                 heroCount++;
         }
     }
-
+    
     void SetButtonState()
     {
         HeroCounting();
         if (heroCount >= 1 && heroCount <= 4)
-            startButtonimage = Managers.Resource.Load<Image>("Images/button2_ready_on");
+            buttonimage = Managers.Resource.Load<Image>("Images/button2_ready_on");
         else
-            startButtonimage = Managers.Resource.Load<Image>("Images/button2_ready_off");
+            buttonimage = Managers.Resource.Load<Image>("Images/button2_ready_off");
     }
 
 }
