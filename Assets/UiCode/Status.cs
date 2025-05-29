ea99ff836be580403e8b461db53a893591910367
kangmunjeong <kmj7232@gmail.com>
2025-05-29 18:09:00 +0900

    Status Ui 수정
    Tactic 볼 수 있게 추가, Image 로딩 문제 해결

--- Assets/UiCode/Status.cs
+++ Assets/UiCode/Status.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
 
 
 public class Status : MonoBehaviour
@@ -16,20 +17,37 @@ public class Status : MonoBehaviour
     public TMP_Text attackRange;
     public TMP_Text moveSpeed;
     public TMP_Text rotationSpeed;
-    
+    public TMP_Text targets;
+    public TMP_Text conditions;
+    public TMP_Text actions;
+
+
     public void Init(CharacterStat data)
     {
         unitData = data;
 
+        unitName.text = this.name = data.DisplayName;
         unitImg.sprite = Managers.Resource.Load<Sprite>($"Character/ScreenShot/{unitName.text}");
         damage.text = $"Damage : {data.damage}";
         attackRange.text = $"AttackRange : {data.attackRange}";
         moveSpeed.text = $"MoveSpeed : {data.moveSpeed}";
         rotationSpeed.text = $"RotationSpeed : {data.rotationSpeed}";
-        unitName.text = this.name = data.DisplayName;
         hp.text = $"{data.hp} / {data.hp_max}";
         mp.text = $"{data.mp} / {data.mp_max}";
         hpSlider.value = (float)data.hp / data.hp_max;
         mpSlider.value = (float)data.mp / data.mp_max;
+        targets.text = $"Targets:\n{string.Join("\n", data.Targets)}";
+        conditions.text = $"\nConditions:\n{string.Join("\n", data.Conditions)}";
+        // Actions 출력
+        List<string> actionDescriptions = new List<string>();
+        foreach (var action in data.Actions)
+        {
+            if (action == null) continue; // 혹시 null 체크
+
+            string desc = $"- {action.displayName}\n   Cooldown: {action.actionCooldown}\n   Effect Value: {data.damage + action.amount}\n   SingleTarget: {action.IsSingleTarget}";
+            actionDescriptions.Add(desc);
+        }
+
+        actions.text = $"\nActions:\n{string.Join("\n", actionDescriptions)}";
     }
 }
